ed6f80f5411c8bae036c17b2657203320aca49d7
Vlad <borov.v1ad@yandex.ru>
2020-10-13 19:06:03 +0300

    Initial project

---
+++ main_service_of_plotters/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from main_service_of_plotters.users.models import User
+from main_service_of_plotters.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
