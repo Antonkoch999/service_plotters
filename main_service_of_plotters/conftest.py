8b3ed32798800f01f447f213db858e48390827c6
Vlad <borov.v1ad@yandex.ru>
2020-10-19 17:34:44 +0300

    Comment conftest.py file

--- main_service_of_plotters/conftest.py
+++ main_service_of_plotters/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 from main_service_of_plotters.apps.users.models import User
-from main_service_of_plotters.apps.users.tests.factories import UserFactory
+# from main_service_of_plotters.apps.users.tests.factories import UserFactory
 
 
 @pytest.fixture(autouse=True)
@@ -9,6 +9,6 @@ def media_storage(settings, tmpdir):
     settings.MEDIA_ROOT = tmpdir.strpath
 
 
-@pytest.fixture
-def user() -> User:
-    return UserFactory()
+# @pytest.fixture
+# def user() -> User:
+#     return UserFactory()
