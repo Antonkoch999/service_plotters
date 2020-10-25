d89c27b3d59308f3e1c09dea256367f690e87416
Anton Kochnevski <Antonkoch999@gmail.com>
2020-10-25 23:41:50 +0300

    'add_photo_models_category'

--- main_service_of_plotters/apps/category/models.py
+++ main_service_of_plotters/apps/category/models.py
@@ -5,6 +5,9 @@ from main_service_of_plotters.utils.abstractmodel import DateTimeDateUpdate
 class DeviceCategory(DateTimeDateUpdate):
     name = models.CharField(max_length=150, blank=True,
                             verbose_name='Name of template device')
+    photo = models.ImageField(upload_to="device/%Y/%m/%d",
+                              verbose_name='Photo Device', blank=True,
+                              null=True)
 
     def __str__(self):
         return f'Category {self.name}'
@@ -16,6 +19,9 @@ class Manufacturer(DateTimeDateUpdate):
                                         verbose_name='Instance model device')
     name = models.CharField(max_length=150, blank=True,
                             verbose_name='Name of template manufacturer')
+    photo = models.ImageField(upload_to="manufacturer/%Y/%m/%d",
+                              verbose_name='Photo manufacturer', blank=True,
+                              null=True)
 
     def __str__(self):
         return f'Category {self.device_category.name} | Manufacturer {self.name}'
