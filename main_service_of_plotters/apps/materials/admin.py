a9a792b894658654451a967c67deb449d940afad
Dzmitry Izaitka <di_devo@inbox.ru>
2020-10-27 12:09:16 +0300

    add commnets to material

--- main_service_of_plotters/apps/materials/admin.py
+++ main_service_of_plotters/apps/materials/admin.py
@@ -1,87 +1,83 @@
+'''Classes and methods of models `Lable` and `Template for admin page.'''
+
 from django.contrib import admin
 from django import forms
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from import_export import resources
 from import_export.admin import ImportExportMixin
-from django.shortcuts import render
-from .forms import SelectUserForm, SelectDealerForm
-from django.http import HttpResponseRedirect
-from main_service_of_plotters.apps.users.models import User
 
+
+from main_service_of_plotters.apps.users.models import User
 from main_service_of_plotters.apps.materials.models import Label, Template
+from .forms import SelectUserForm, SelectDealerForm, LabelFormDealer, LabelFormUser
 
 
 class LabelResource(resources.ModelResource):
+    '''Model Resourse for django-import-export.'''
+
     class Meta:
         model = Label
 
 
-class LabelFormDealer(forms.ModelForm):
-    class Meta:
-        models = Label
-        exclude = ('dealer', 'scratch_code')
-
-
-class LabelFormUser(forms.ModelForm):
-    class Meta:
-        models = Label
-        exclude = ('user', 'dealer', 'scratch_code')
-
-
 class CustomLabelAdmin(ImportExportMixin, admin.ModelAdmin):
+    '''`Label` admin page.'''
+
+    # import resource class for working django-import-export
     resource_class = LabelResource
     list_display = ('scratch_code', 'barcode', 'date_creation', 'date_update',
                     'count', 'dealer', 'user')
     list_filter = ('date_creation',)
-    search_fields = ('date_creation',)
+    search_fields = ('barcode',)
+    # custom actions
     actions = ['add_user', 'add_dealer']
 
-    def add_user(self, request, queryset):
-        form = None
+    def _add_generic_user(self, request, queryset,
+                          instance_field_name: str,
+                          user_form: forms.Form,
+                          template_path: str):
+        '''Generic method ads action views for two actions below.'''
 
+        form = None
+        # Actions when pressed 'submit'
         if 'apply' in request.POST:
-            form = SelectUserForm(request.POST)
+            # Get form for request
+            form = user_form(request.POST)
 
             if form.is_valid():
-                user = form.cleaned_data['user']
+                # get choosen instance to add in set of labels
+                instance_field = form.cleaned_data[instance_field_name]
 
+                # update all of labels in instance (owner) field
                 for item in queryset:
-                    item.user = user
+                    item_changing_field = item.get_field(instance_field_name)
+                    item_changing_field = instance_field
                     item.save()
 
                 return HttpResponseRedirect(request.get_full_path())
-
+        # Actions when action is choosed
         if not form:
-            form = SelectUserForm(initial={
+            # render view with form and template
+            form = user_form(initial={
                 '_selected_action': request.POST.getlist(
                     admin.ACTION_CHECKBOX_NAME)})
-            return render(request, 'admin/multiple_owner_change.html',
+            return render(request, template_path,
                           {'items': queryset, 'form': form,
-                           'title': u'Изменение категории'})
+                           'title': f'Change {instance_field_name} for multiple labels'})
+
+    def add_user(self, request, queryset):
+        '''Action to add user as owner for set of labels.'''
+
+        return self._add_generic_user(request, queryset, 'user', SelectUserForm, 'admin/multiple_owner_change.html')
 
     def add_dealer(self, request, queryset):
-        form = None
+        '''Action toi add dealer as owner for set in labels.'''
 
-        if 'apply' in request.POST:
-            form = SelectDealerForm(request.POST)
-
-            if form.is_valid():
-                dealer = form.cleaned_data['dealer']
-
-                for item in queryset:
-                    item.dealer = dealer
-                    item.save()
-
-                return HttpResponseRedirect(request.get_full_path())
-
-        if not form:
-            form = SelectDealerForm(initial={
-                '_selected_action': request.POST.getlist(
-                    admin.ACTION_CHECKBOX_NAME)})
-            return render(request, 'admin/add_dealer.html',
-                          {'items': queryset, 'form': form,
-                           'title': u'Изменение категории'})
+        return self._add_generic_user(request, queryset, 'dealer', SelectDealerForm, 'admin/add_dealer.html')
 
     def get_actions(self, request):
+        '''Change list of actions for different users'''
+
         actions = super().get_actions(request)
         if request.user.groups.filter(name='Dealer').exists():
             del actions['add_dealer']
@@ -90,19 +86,27 @@ class CustomLabelAdmin(ImportExportMixin, admin.ModelAdmin):
         return actions
 
     def get_queryset(self, request):
+        '''Change list of available labels depended of logged user.'''
+
         qs = super().get_queryset(request)
+        # Dealer can see own labels
         if request.user.groups.filter(name='Dealer').exists():
             return qs.filter(dealer=request.user.pk)
+        # User can see only own labels
         elif request.user.groups.filter(name='User').exists():
             return qs.filter(user=request.user.pk)
         return qs
 
     def get_form(self, request, obj=None, **kwargs):
+        '''Change form of admin page depended of logged user.'''
+
         if request.user.role == 'Dealer':
             kwargs['form'] = LabelFormDealer
+            # Dealer can add to label only user it own
             form = super().get_form(request, obj, **kwargs)
             form.base_fields['user'].queryset = User.objects.filter(
                 dealer_id=request.user.pk)
+            # For dealer barcode is unchangable
             form.base_fields['barcode'].disabled = True
             return form
         elif request.user.role == 'User':
@@ -110,14 +114,25 @@ class CustomLabelAdmin(ImportExportMixin, admin.ModelAdmin):
         return super().get_form(request, obj, **kwargs)
 
     def get_list_display(self, request):
-        if request.user.groups.filter(name='Dealer').exists() \
-                or request.user.groups.filter(name='User').exists():
+        '''Change list_display list depended of logged user.'''
+
+        # If user is `Dealer` or User
+        if CustomLabelAdmin._is_requested_user_dealer_or_user(request):
+            # without `scretch code`
             return ['barcode', 'date_creation', 'date_update',
                     'count', 'dealer', 'user']
         return super().get_list_display(request)
 
+    @staticmethod
+    def _is_requested_user_dealer_or_user(request):
+        '''Helper method identificate is authenticated user is dealer.'''
+
+        return request.user.groups.filter(name='Dealer').exists() \
+            or request.user.groups.filter(name='User').exists()
+
 
 class TemplateAdmin(admin.ModelAdmin):
+    '''Class for `Template` admin representation.'''
     list_display = ('name', 'device_category', 'manufacturer_category',
                     'file_photo', 'file_plt', 'date_creation', 'date_update')
 
