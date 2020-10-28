e2768fadde97ceed84396c8bcfa5f6d522e07021
Dzmitry Izaitka <di_devo@inbox.ru>
2020-10-28 11:25:44 +0300

    validators pycodestyle

--- main_service_of_plotters/apps/materials/validators.py
+++ main_service_of_plotters/apps/materials/validators.py
@@ -8,7 +8,6 @@ from django.utils.translation import gettext_lazy
 
 def validate_unique_code(value):
     """Checks numbers and their number."""
-
     if not value.isdigit():
         raise ValidationError(
             gettext_lazy(f'{value} is not number')
@@ -21,7 +20,6 @@ def validate_unique_code(value):
 
 def validate_file_plt(value):
     """Checks the file format."""
-
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
     valid_extensions = ['.plt', ]
     if not ext.lower() in valid_extensions:
@@ -30,7 +28,6 @@ def validate_file_plt(value):
 
 def validate_file_photo(value):
     """Checks the file format."""
-
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
     valid_extensions = ['.png', '.svg']
     if not ext.lower() in valid_extensions:
