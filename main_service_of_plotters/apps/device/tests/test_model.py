37e09b451e27c2f9159d07062f3d2666d95c97c5
Dzmitry Izaitka <di_devo@inbox.ru>
2020-10-28 12:14:17 +0300

    initial plotter test

--- main_service_of_plotters/apps/device/tests/test_model.py
+++ main_service_of_plotters/apps/device/tests/test_model.py
@@ -1,7 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from main_service_of_plotters.apps.device.models import Plotter
 from main_service_of_plotters.apps.users.models import User
+from ...users.constants import ROLE
 
 
 class TestModelPlotter(TestCase):
@@ -20,3 +21,39 @@ class TestModelPlotter(TestCase):
 
     def test_model_plotter_count_number(self):
         self.assertEqual(len(str(self.plotter.serial_number)), 16)
+
+    def test_model_plotter_str(self):
+        self.assertEqual(str(self.plotter), 'Plotter 1111222233334444')
+
+
+class TestAdminPlotter(TestCase):
+    """Testing plotter admin page."""
+
+    def setUp(self):
+        """"Setting up environment."""
+        self.superuser = User.objects.create_superuser(
+            username='admin',
+            password='admin'
+        )
+        self.user1 = User.objects.create(
+            username='user1',
+            password='user1'
+        )
+        self.user2 = User.objects.create(
+            username='user2',
+            password='user2'
+        )
+        self.admin = User.objects.create(
+            username='admin',
+            password='admin',
+            role=ROLE['Administrator']
+        )
+        self.dealer1 = User.objects.create(
+            username='dealer',
+            password='dealer'
+            role=ROLE['Dealer']
+        )
+        self.plotter1 = Plotter(user=None, dealer=None, serial_number=1)
+        self.c = Client()
+
+
