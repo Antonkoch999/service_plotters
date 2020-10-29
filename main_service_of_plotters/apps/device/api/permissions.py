a087925bc568594178885b99ce533cbfc233ec58
Dzmitry Izaitka <di_devo@inbox.ru>
2020-10-29 15:17:44 +0300

    plotter api viewset superadmin perm fix

--- main_service_of_plotters/apps/device/api/permissions.py
+++ main_service_of_plotters/apps/device/api/permissions.py
@@ -5,18 +5,19 @@ class PlotterUserPermission(BasePermission):
     """Permission check request user to iteracte with plotters."""
 
     def has_permission(self, request, view):
-        print('perm')
         if request.method in SAFE_METHODS:
             return True
         else:
             return request.user.is_dealer() or \
-                request.user.is_administrator()
+                request.user.is_administrator() or \
+                request.user.is_superuser()
 
     def has_object_permission(self, request, view, obj):
-        print('obj perm')
         if request.user.is_user():
             return obj.user == request.user
         elif request.user.is_dealer():
             return obj.dealer == request.user
         elif request.user.is_administrator():
             return True
+        else:
+            return request.user.is_superuser()
