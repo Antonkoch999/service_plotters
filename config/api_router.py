cb9f530b4031b30993f14d8d2eabd85010b8b4c6
Anton <antonrobotics@gmail.com>
2020-10-29 12:45:25 +0300

    'users_api'

--- config/api_router.py
+++ config/api_router.py
@@ -1,15 +1,21 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
-
-from main_service_of_plotters.apps.users.api.views import UserViewSet
+from django.urls import include, path
+from django.conf.urls import url
+from main_service_of_plotters.apps.users.api.views import (
+    api_root, UserListView, UserRegisterView, UserDetailView)
 
 if settings.DEBUG:
     router = DefaultRouter()
 else:
     router = SimpleRouter()
 
-router.register("users", UserViewSet)
-
 
 app_name = "api"
-urlpatterns = router.urls
+urlpatterns = [
+    path('', api_root),
+    path('users', UserListView.as_view(), name='user'),
+    path('registrations/', UserRegisterView.as_view(), name='registrations'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail')
+]
+
