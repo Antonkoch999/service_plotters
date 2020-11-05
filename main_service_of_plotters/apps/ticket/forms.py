c7154cc5ad118851ba8aeaa8c73d7a4543f840ef
Dzmitry Izaitka <di_devo@inbox.ru>
2020-11-05 18:34:08 +0300

    first view

---
+++ main_service_of_plotters/apps/ticket/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+from .models import Ticket, PopularProblem
+from main_service_of_plotters.apps.device.models import Plotter
+
+WARIANT_NOT_PRESENTED = 'wariant_not_presented'
+
+
+def _list_of_popular_problems_plus_not_found():
+        choices = [
+            (f"{pp.id}", pp.name)
+            for pp
+            in PopularProblem.objects.all()
+        ]
+        choices.append((WARIANT_NOT_PRESENTED, _("My problem is not presented in this list")))
+        return choices
+
+
+class ChoosePopularProblemForm(forms.Form):
+    plotters = forms.ModelMultipleChoiceField(queryset=Plotter.objects)
+    problem = forms.ChoiceField(
+        choices=_list_of_popular_problems_plus_not_found
+    )
+
+    def __init__(self, *args, context=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if context is not None:
+            request = context.get('request')
+            self.fields['plotters'].queryset = request.user.plotter_user
+
+
--- main_service_of_plotters/apps/ticket/views.py
+++ main_service_of_plotters/apps/ticket/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.views.generic.edit import CreateView
 
-from .models import Ticket
-from .froms import ChoosePopularProblemForm
+from .models import Ticket, PopularProblem
+from .forms import ChoosePopularProblemForm, WARIANT_NOT_PRESENTED
+from main_service_of_plotters.apps.device.models import Plotter
+
+STEP_1_ACTION = 'step_1'
 
 
 # Create your views here.
@@ -11,8 +14,36 @@ class UserAddTicket(View):
     def get(self, request):
         form = ChoosePopularProblemForm(context={'request': request})
         context = {'form': form}
+        context['action'] = STEP_1_ACTION
         return render(
             request,
             template_name='ticket/add_ticket.html',
             context=context
         )
+
+    def post(self, request):
+        print(request.POST)
+        action = request.POST.get('action')
+        if action is not None \
+                and action[0] == STEP_1_ACTION\
+                and problem[0] == WARIANT_NOT_PRESENTED:
+            # Do things when user chose not presented problemn
+            pass
+        else:
+            # Choosed popular problem, populate it
+            form = ChoosePopularProblemForm(request.POST)
+            if form.is_valid():
+                chosed_problem = get_object_or_404(
+                    PopularProblem,
+                    pk=form.cleaned_data['problem']
+                )
+                ticket = Ticket.objects.create(
+                    header=chosed_problem.populated_header,
+                    text=chosed_problem.populated_text,
+                    reporter=request.user
+                )
+                ticket.plotters.set(form.cleaned_data["plotters"])
+
+                return redirect('tickets:user_add_ticket')
+            else:
+                return redirect('tickets:user_add_ticket')
