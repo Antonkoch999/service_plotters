5a2d9bfa41a340d6148ade6dc46caceedd5c5561
Dzmitry Izaitka <di_devo@inbox.ru>
2020-11-05 13:21:54 +0300

    add class popular porblem

--- main_service_of_plotters/apps/ticket/models.py
+++ main_service_of_plotters/apps/ticket/models.py
@@ -8,11 +8,16 @@ from main_service_of_plotters.apps.users.models import User
 from main_service_of_plotters.apps.device.models import Plotter
 from .constants import StatusVariants
 
+
+HEADER_MAX_LENGTH = 50
+
+
 class Ticket(DateTimeDateUpdate):
     """Request for a technical support with description of the problem."""
+
     header = models.CharField(
         verbose_name=_("Header"),
-        max_length=50,
+        max_length=HEADER_MAX_LENGTH,
         blank=False,
         help_text=_("Short description of problem.")
     )
@@ -62,13 +67,32 @@ class Ticket(DateTimeDateUpdate):
     )
 
     class Meta:
+
         verbose_name = _("Ticket")
         verbose_name_plural = _("Tickets")
 
 
-# class PopularProblem(models.Model):
-#     name = models.CharField(
-#         verbose_name=_("Name"),
-#         max_length=100,
-#         help_text=_("Short name of ")
-    # )
+class PopularProblem(models.Model):
+    """Model descride list with popular problems."""
+
+    name = models.CharField(
+        verbose_name=_("Name"),
+        max_length=100,
+        help_text=_("Short name of porblem, will be displaied"),
+        blank=True
+    )
+    populated_header = models.CharField(
+        verbose_name=_("Populated Header"),
+        max_length=HEADER_MAX_LENGTH,
+        blank=False,
+        help_text=_("Text what will be populated in created ticket header")
+    )
+    populated_text = models.TextField(
+        verbose_name=_("Populated Text"),
+        help_text=_("Text what will be populated in created ticket text")
+    )
+
+    class Meta:
+
+        verbose_name = _('Popular problem')
+        verbose_name_plural = _('Popular problems')
