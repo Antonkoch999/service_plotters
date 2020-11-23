064f1aeb2d98c7b51f1a92d9d848b0bfb2f20d17
Dzmitry Izaitka <di_devo@inbox.ru>
2020-11-23 17:46:13 +0300

    login fancy views

--- main_service_of_plotters/apps/users/urls.py
+++ main_service_of_plotters/apps/users/urls.py
@@ -1,5 +1,14 @@
-"""This module contains URL patterns."""
+from django.urls import path
+
+from main_service_of_plotters.apps.users.views import (
+    user_detail_view,
+    user_redirect_view,
+    user_update_view,
+)
 
 app_name = "users"
 urlpatterns = [
+    path("~redirect/", view=user_redirect_view, name="redirect"),
+    path("~update/", view=user_update_view, name="update"),
+    path("<str:username>/", view=user_detail_view, name="detail"),
 ]
